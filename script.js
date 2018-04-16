4b5863d25f9cc27a1773380d8efe0e0cbd0b095d
Alex Gjorvad <alexgjorvad@gmail.com>
2018-04-16 09:33:10 -0500

    assignment finished

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+class Employee{
+    constructor(firstName, lastName, id, title, salary){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
+    this.title = title;
+    this.salary = salary;
+}
+}
+
+employees = [];
+
 $(document).ready(function(){
     $('#submitButton').on( 'click', addNewEmployee);
     $('#submitButton').on( 'click', annualSalary);
@@ -11,7 +23,9 @@ function addNewEmployee(){
         '<td>' + $("#newID").val() + '</td>' +
         '<td>' + $("#newTitle").val() + '</td>' +
         '<td>' + '$' + $("#newAnnualSalary").val() + '</td>' +
+        '<td>' + '<button class="deleteButton">Delete</button>' + '</td>' +
     '</tr>');
+    addArray();
 }
 
 let expenditures = 0;
@@ -20,12 +34,19 @@ function annualSalary(){
     let salary = $('#newAnnualSalary').val();
     let month = salary / 12; 
     expenditures += month;
-    $('#monthlyExpenditures').text('Monthly Total: ' + expenditures);
+    $('#monthlyExpenditures').text( 'Total Monthly: ' + '$' + expenditures.toFixed(2));
+    if (expenditures > 20000){
+        $('#monthlyExpenditures').css( 'color', 'red');
+    }
+
+$('#employeeList').on( 'click', '.deleteButton', function(){
+    console.log( 'Delete button was clicked.');
+    $(this).closest('tr').remove();
+});
 }
 
-function blah(){
-    $("#newFirstName, #newLastName, #newID, #newTitle, #newAnnualSalary").val('');
+function addArray(){
+let newEmployee = new Employee($("#newFirstName").val(), $("#newLastName").val(), $("#newID").val(), $("#newTitle").val(), $("#newAnnualSalary").val());
+    employees.push( newEmployee );
+    console.log( employees );
 }
-
-
-
